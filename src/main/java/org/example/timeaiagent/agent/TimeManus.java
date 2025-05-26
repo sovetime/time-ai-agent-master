01daf5c5c37cfce0e99b31bba38bc455f494dd16
time <2279498816@qq.com>
2025-05-26 22:12:27 +0800

    智能体改进

--- src/main/java/org/example/timeaiagent/agent/TimeManus.java
+++ src/main/java/org/example/timeaiagent/agent/TimeManus.java
@@ -16,13 +16,19 @@ public class TimeManus extends ToolCallAgent {
         this.setName("TimeManus");
 
         //系统提示词
+        //你是TimeManus，一个全能的人工智能助手，旨在解决用户提出的任何任务。
+        //您可以使用各种工具来高效地完成复杂的请求
         String SYSTEM_PROMPT = """
-                You are YuManus, an all-capable AI assistant, aimed at solving any task presented by the user.
+                You are TimeManus, an all-capable AI assistant, aimed at solving any task presented by the user.
                 You have various tools at your disposal that you can call upon to efficiently complete complex requests.
                 """;
         this.setSystemPrompt(SYSTEM_PROMPT);
 
         //下一步提示词
+        //根据用户需求，主动选择最合适的工具或工具组合。
+        //对于复杂的任务，您可以分解问题并逐步使用不同的工具来解决它。
+        //使用每个工具后，清楚地解释执行结果并建议下一步。
+        //如果你想在任何时候停止交互，请使用“terminate”工具/函数调用
         String NEXT_STEP_PROMPT = """
                 Based on user needs, proactively select the most appropriate tool or combination of tools.
                 For complex tasks, you can break down the problem and use different tools step by step to solve it.
